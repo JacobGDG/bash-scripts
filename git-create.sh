88d54d0cd96eeb3f1983fc8f0d9071ff6846763c
JacobGDG <jacob.greenwood@gmail.com>
2021-02-20 15:36:46 +0000

    Initial Commit
    A hub for my bash-scripts. Created with my first bash script.
    
    \# git-create.sh
    Takes a string as input and makes a new repo in JacobGDG area. Requires
    `$GIT_TOKEN` to be set to auth token.

---
+++ git-create.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+repo_name=$1
+if [ -z $repo_name ]
+then
+  echo "Repo name required." && exit 1
+fi
+if [ -z $GIT_TOKEN ]
+then
+  echo "GIT_TOKEN must be set." && exit 1
+fi
+
+curl -H "Authorization: token $GIT_TOKEN" https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
+
+git init
+git remote add origin "https://github.com/JacobGDG/$repo_name.git"
